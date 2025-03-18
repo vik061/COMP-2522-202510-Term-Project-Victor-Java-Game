ec360c3436d20e59228dd036b1e36925e939fdfd
Victor Lum <vlum8@my.bcit.ca>
2025-03-17 22:45:39 -0700

    Add isStart, isEnd, isOmnipotent variables to Space

--- src/main/java/com/example/comp2522202510termprojectvictorjavagame/Space.java
+++ src/main/java/com/example/comp2522202510termprojectvictorjavagame/Space.java
@@ -4,10 +4,19 @@ import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 
 public class Space extends Rectangle {
+    private int position;
+    protected boolean isStart;
+    protected boolean isEnd;
+    protected boolean isOmnipotentSix;
 
-    public Space(int position) {
+    public Space(int position, boolean isStart, boolean isEnd, boolean isOmnipotentSix) {
         setWidth(position);
         setHeight(position);
+        this.position = position;
+        this.isStart = position == 1;
+        this.isEnd = position == 100;
+        this.isOmnipotentSix = position % 6 == 0;
+
         setFill(Color.AQUAMARINE);
         setStroke(Color.BLACK);
     }
