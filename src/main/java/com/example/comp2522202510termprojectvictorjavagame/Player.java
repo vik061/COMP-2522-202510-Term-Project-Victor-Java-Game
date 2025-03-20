314a2f3c8a088d6b64c9c56f4a4337f0d14849b9
Victor Lum <vlum8@my.bcit.ca>
2025-03-20 16:46:43 -0700

    Adjust Player constructor, variables, setPlayerPosition, getter methods

--- src/main/java/com/example/comp2522202510termprojectvictorjavagame/Player.java
+++ src/main/java/com/example/comp2522202510termprojectvictorjavagame/Player.java
@@ -1,19 +1,47 @@
 package com.example.comp2522202510termprojectvictorjavagame;
 
-public class Player {
-    int position;
-    boolean finishedGame;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
 
-    public Player(int position, boolean finishedGame) {
-        this.position = position;
-        this.finishedGame = position == 100;
+
+public class Player {
+    private Circle playerPiece;
+    private int position;
+    private boolean finishedGame;
+    private static Board gameBoard = new Board();
+    private static final int winNumber = 100;
+
+    public Player(int spaceSize, final Color pieceColor) {
+        playerPiece = new Circle(spaceSize / 2);
+        playerPiece.setFill(pieceColor);
+        position = 1;
+        finishedGame = (position == winNumber);
     }
 
     public int getPlayerPosition() {
         return position;
     }
 
-    public void setPlayerPosition(int newPosition) {
-        this.position = newPosition;
+    public void setPlayerPosition(final int dieValue) {
+        if (position + dieValue <= winNumber) {
+            position += dieValue;
+        }
+        int xCoord = gameBoard.getXCoordinate(position);
+        int yCoord = gameBoard.getYCoordinate(position);
+        playerPiece.setTranslateX(xCoord);
+        playerPiece.setTranslateY(yCoord);
+
+    }
+
+    public Circle getPlayerPiece() {
+        return playerPiece;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
+    public boolean isFinishedGame() {
+        return finishedGame;
     }
 }
