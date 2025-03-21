385e6c20d7621e5e0971d5f99c9bba0526cb56b3
Victor Lum <vlum8@my.bcit.ca>
2025-03-20 17:23:22 -0700

    Adjust createContent code with Player piece and Button action

--- src/main/java/com/example/comp2522202510termprojectvictorjavagame/Game.java
+++ src/main/java/com/example/comp2522202510termprojectvictorjavagame/Game.java
@@ -1,7 +1,10 @@
 package com.example.comp2522202510termprojectvictorjavagame;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.control.Button;
+import javafx.scene.paint.Color;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
@@ -15,6 +18,7 @@ public class Game extends Application {
     public static final int spaceSize=40, width=10, height=10;
     public static final int buttonLine = height * spaceSize + 35;
     public static final int descriptionLine = buttonLine - 20;
+    private Player playerOne;
 
     private Pane createContent() {
         Pane root = new Pane();
@@ -22,7 +26,7 @@ public class Game extends Application {
 
         for (int index = 0; index < height; index++) {
             for (int j_index = 0; j_index < width; j_index++) {
-                Space space = new Space(spaceSize);
+                Space space = new Space(spaceSize, false, false, false);
                 space.SetTranslateX(j_index * spaceSize);
                 space.SetTranslateY(index * spaceSize);
                 root.getChildren().addAll(space);
@@ -41,32 +45,44 @@ public class Game extends Application {
 
 
         playerButton.setTranslateY(buttonLine);
-        playerButton.setTranslateX(45);
-        diceButton.setTranslateY(buttonLine);
-        diceButton.setTranslateX(160);
-        startButton.setTranslateY(buttonLine);
-        startButton.setTranslateX(270);
+        playerButton.setTranslateX(160);
+//        diceButton.setTranslateY(buttonLine);
+//        diceButton.setTranslateX(160);
+//        startButton.setTranslateY(buttonLine);
+//        startButton.setTranslateX(270);
 
         Label playerLabel = new Label("Your Turn");
-        Label diceLabel = new Label("Dice");
-        Label startLabel = new Label("Start");
+//        Label diceLabel = new Label("Dice");
+//        Label startLabel = new Label("Start");
 
         playerLabel.setTranslateY(descriptionLine);
-        playerLabel.setTranslateX(55);
-        diceLabel.setTranslateY(descriptionLine);
-        diceLabel.setTranslateX(175);
-        startLabel.setTranslateY(descriptionLine);
-        startLabel.setTranslateX(277);
+        playerLabel.setTranslateX(171);
+//        diceLabel.setTranslateY(descriptionLine);
+//        diceLabel.setTranslateX(175);
+//        startLabel.setTranslateY(descriptionLine);
+//        startLabel.setTranslateX(277);
 
-        diceButton.setOnAction(event -> {
-            int value = Dice.rollDice();
-            System.out.println("Dice: " + value);
-            diceLabel.setText("Dice: " + value);
+//        diceButton.setOnAction(event -> {
+//            int value = Dice.rollDice();
+//            System.out.println("Dice: " + value);
+//            diceLabel.setText("Dice: " + value);
+//        });
+
+        playerOne = new Player(spaceSize, Color.BLACK);
+
+        playerButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(final ActionEvent actionEvent) {
+                int dieValue = Dice.rollDice();
+                playerLabel.setText("Dice: " + dieValue);
+                playerOne.setPlayerPosition(dieValue);
+            }
         });
 
         root.getChildren().addAll(gameBoard,
-                playerButton, diceButton, startButton,
-                playerLabel, diceLabel, startLabel);
+                playerButton,
+                playerLabel,
+                playerOne.getPlayerPiece());
 
         return root;
     }
