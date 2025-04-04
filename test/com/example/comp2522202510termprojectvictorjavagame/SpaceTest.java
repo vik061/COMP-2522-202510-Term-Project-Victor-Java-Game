901509b118e1880b1a2637cc7460fd1a412381c4
Victor Lum <vlum8@my.bcit.ca>
2025-04-04 12:28:20 -0700

    Sprint-05 Add unit tests

--- test/com/example/comp2522202510termprojectvictorjavagame/SpaceTest.java
+++ test/com/example/comp2522202510termprojectvictorjavagame/SpaceTest.java
@@ -1,16 +1,61 @@
 package com.example.comp2522202510termprojectvictorjavagame;
 
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 class SpaceTest {
+    private final int position = 50;
+    Space space = new Space(position);
 
     @Test
-    void setTranslateX() {
+    void testConstructorInitialization() {
+        int currentPosition = space.getPosition();
+        double width = space.getWidth();
+        double height = space.getHeight();
+
+        assertEquals(position, currentPosition);
+        assertEquals(position, width);
+        assertEquals(position, height);
     }
 
     @Test
-    void setTranslateY() {
+    void testConstructorEdgeCaseZero() {
+        Space zeroSpace = new Space(0);
+        assertEquals(0, zeroSpace.getPosition());
+    }
+
+    @Test
+    void testConstructorEdgeCaseMaxNumber() {
+        Space maxSpace = new Space(Integer.MAX_VALUE);
+        assertEquals(Integer.MAX_VALUE, maxSpace.getPosition());
+    }
+
+    @Test
+    void testSetTranslateX() {
+        space.setTranslateX(100);
+        assertEquals(100, space.getTranslateX());
+    }
+
+    @Test
+    void testSetTranslateXNegativeValue() {
+        space.setTranslateX(-50);
+        assertEquals(-50, space.getTranslateX());
+    }
+
+    @Test
+    void testSetTranslateY() {
+        space.setTranslateY(200);
+        assertEquals(200, space.getTranslateY());
+    }
+
+    @Test
+    void testSetTranslateYNegativeValue() {
+        space.setTranslateY(-100);
+        assertEquals(-100, space.getTranslateY());
+    }
+
+    @Test
+    void testGetPosition() {
+        assertEquals(50, space.getPosition());
     }
 }
