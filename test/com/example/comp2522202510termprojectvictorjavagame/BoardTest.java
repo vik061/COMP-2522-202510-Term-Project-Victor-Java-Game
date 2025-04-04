d3cc6d392e47e45bfa679f27bbe81ab364c6cc6e
Victor Lum <vlum8@my.bcit.ca>
2025-04-03 22:00:45 -0700

    Sprint-05 Create unit tests

--- test/com/example/comp2522202510termprojectvictorjavagame/BoardTest.java
+++ test/com/example/comp2522202510termprojectvictorjavagame/BoardTest.java
@@ -4,17 +4,51 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class BoardTest {
+public class BoardTest {
+    private final int validNonOmnipotentPosition = 11;
+    private final int invalidNonOmnipotentPosition = 101;
+    Board board = new Board();
 
     @Test
-    void getNewPosition() {
+    void testGetNewPositionNonOmnipotentPositionValidRange() {
+        int newPosition = board.getNewPosition(validNonOmnipotentPosition);
+        assertEquals(validNonOmnipotentPosition, newPosition);
     }
 
     @Test
-    void getXCoordinate() {
+    void testGetNewPositionOmnipotentPositionValidRange() {
+        int validOmnipotentPosition = 12;
+        int newPosition = board.getNewPosition(validOmnipotentPosition);
+        assertTrue(true, String.valueOf(newPosition >= 7 && newPosition <= 17));
     }
 
     @Test
-    void getYCoordinate() {
+    void testGetNewPositionInvalidfRange() {
+        int newPosition = board.getNewPosition(invalidNonOmnipotentPosition);
+        assertEquals(-1, newPosition);
+    }
+
+    @Test
+    void testGetXCoordinateValidRange() {
+        int xCoordinate = board.getXCoordinate(validNonOmnipotentPosition);
+        assertEquals(380, xCoordinate);
+    }
+
+    @Test
+    void testGetXCoordinateInvalidRange() {
+        int xCoordinate = board.getXCoordinate(invalidNonOmnipotentPosition);
+        assertEquals(-1, xCoordinate);
+    }
+
+    @Test
+    void testGetYCoordinateValidRange() {
+        int yCoordinate = board.getYCoordinate(validNonOmnipotentPosition);
+        assertEquals(340, yCoordinate);
+    }
+
+    @Test
+    void testGetYCoordinateInvalidRange() {
+        int yCoordinate = board.getYCoordinate(invalidNonOmnipotentPosition);
+        assertEquals(-1, yCoordinate);
     }
 }
