2217bd89b829be5b8925f8ddee5f141c27fc4b40
Victor Lum <vlum8@my.bcit.ca>
2025-04-06 21:56:43 -0700

    Sprint-05 Create variables for each unit test

--- test/com/example/comp2522202510termprojectvictorjavagame/GameTest.java
+++ test/com/example/comp2522202510termprojectvictorjavagame/GameTest.java
@@ -3,32 +3,23 @@ package com.example.comp2522202510termprojectvictorjavagame;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
-import javafx.application.Platform;
-import org.junit.jupiter.api.BeforeAll;
-
 
 public class GameTest {
-    @BeforeAll
-    static void initJavaFX() {
-        try {
-            Platform.startup(() -> {});
-        } catch (IllegalStateException error) {
-            error.printStackTrace();
-        }
-    }
-
     @Test
     void testGetSpaceSize() {
-        assertEquals(40, Game.getSpaceSize());
+        int spaceSize = 40;
+        assertEquals(spaceSize, Game.getSpaceSize());
     }
 
     @Test
     void testGetWidth() {
-        assertEquals(10, Game.getWidth());
+        int width = 10;
+        assertEquals(width, Game.getWidth());
     }
 
     @Test
     void testGetHeight() {
-        assertEquals(10, Game.getHeight());
+        int height = 10;
+        assertEquals(height, Game.getHeight());
     }
 }
